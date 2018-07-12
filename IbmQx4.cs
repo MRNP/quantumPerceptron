861e3d90448bdb216a33489c895da5530e3ed409
Filippo Valle <filippo.valle@edu.unito.it>
2018-07-12 17:18:05 +0200

    add Qiskit

---
+++ IbmQx4.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+namespace progettoreti
+{
+    /*
+     * Quick and dirty driver to enable the IbmQx4
+     */
+    class IbmQx4 : QiskitDriver
+    {
+        public IbmQx4(string key) : base(key)
+        {
+        }
+
+        public override int QBitCount => 5;
+
+        public override string Name => "ibmqx4";
+    }
+}
